4bbfb9c65ff20a30879959da2fb29e78bcb29d43
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-06-30 23:29:04 -0500

    revert types

--- asgi_background/_types.py
+++ asgi_background/_types.py
@@ -1,10 +1,4 @@
-import sys
 import typing
-
-if sys.version_info < (3, 8):
-    from typing_extensions import Protocol
-else:
-    from typing import Protocol
 
 Scope = typing.MutableMapping[str, typing.Any]
 Message = typing.MutableMapping[str, typing.Any]
@@ -12,9 +6,4 @@ Message = typing.MutableMapping[str, typing.Any]
 Receive = typing.Callable[[], typing.Awaitable[Message]]
 Send = typing.Callable[[Message], typing.Awaitable[None]]
 
-
-class ASGIApp(Protocol):
-    def __call__(
-        self, __scope: Scope, __receive: Receive, __send: Send
-    ) -> typing.Awaitable[None]:
-        ...
+ASGIApp = typing.Callable[[Scope, Receive, Send], typing.Awaitable[None]]
